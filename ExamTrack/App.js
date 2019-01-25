c195a350d0c8308348795ea0bc697d8ce23e9bb0
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-01-25 11:57:07 -0500

    Updated App.js
     - Added icons
     - Updated Event class to now include a state of whether it is complete

--- ExamTrack/App.js
+++ ExamTrack/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, AppRegistry, ScrollView} from 'react-native';
+import {StyleSheet, Text, View, AppRegistry, ScrollView, Image} from 'react-native';
+
+export const CHECK = require('./icons/check.png');
+export const NOTCHECK = require('./icons/notcheck.png');
 
 class Event extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {complete: false};
+  }
+
+  render() {
+
+    if (this.state.complete) {
+      return(
+      <View style={EventContainerStyles.item}>
+        <Image style={{width: 25, height: 25}} source={CHECK} />
+        <Text> To Do: {this.props.name} </Text>
+      </View>);
+    } else {
+      return(
+      <View style={EventContainerStyles.item}>
+        <Image style={{width: 25, height: 25}} source={NOTCHECK} />
+        <Text> To Do: {this.props.name} </Text>
+      </View>);
+    }
+  }
+
+}
+
+class Title extends Component {
   render() {
     return (
       <View style={{alignItems: 'center'}}>
-        <Text> To Do: {this.props.name} </Text>
+        <Text> Exam Track </Text>
       </View>
     );
   }
@@ -23,11 +52,18 @@ export default class EventsList extends Component {
   }
 }
 
-// const scrollStyles = StyleSheet.create({
-//   contentContainer: {
-//     paddingVertical: 20
-//   }
-// });
+const EventContainerStyles = StyleSheet.create({
+  item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 20,
+        margin: 5,
+        borderColor: '#000000',
+        borderWidth: 2,
+        backgroundColor: '#ffffff'
+     }
+});
 //
 // const styles = StyleSheet.create({
 //   container: {
